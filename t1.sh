325aa635cd2299abbbee31b700d6c67d89615aef
kavyameenus <lkavya805>
2019-04-03 11:26:07 +0530

    trade

---
+++ t1.sh
@@ -0,0 +1,19 @@
+!/bin/bash
+
+echo "enter the first string :"
+
+read str1
+
+echo "enter the second string :"
+
+read str2
+
+if [ "$str1" == "$str2" ];then
+
+echo "strings are equal"
+
+else
+
+echo "strings are not equal"
+
+fi 
